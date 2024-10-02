ddf76e55bb3ddd3256f1c0b86ac42d41283cc418
stelasusanina <114248714+stelasusanina@users.noreply.github.com>
2024-10-02 15:16:27 +0300

    Page not found scenario added

--- ExpressFirstApp/index.js
+++ ExpressFirstApp/index.js
@@ -2,13 +2,18 @@ const express = require("express");
 const staticRoutes = require("./routes/staticRoutes");
 const app = express();
 
-app.use("/static", staticRoutes);
-
 app.use("/static", express.static("public"));
+app.use("/static", staticRoutes);
 
 app.get("/", (req, res) => {
   res.set({ "Content-Type": "text/html" });
   res.send("Hi");
+});
+
+app.use((req, res) => {
+  res
+    .status(404)
+    .sendFile("public/notFoundErrorPage.html", { root: __dirname });
 });
 
 app.listen(3000);
