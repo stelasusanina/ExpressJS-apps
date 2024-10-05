4f250cdc8ece6243525081be517c8161381dc432
stelasusanina <114248714+stelasusanina@users.noreply.github.com>
2024-10-05 22:22:29 +0300

    Updated content-type and cors to update dynamically

--- ExpressFirstApp/routes/staticRoutes.js
+++ ExpressFirstApp/routes/staticRoutes.js
@@ -1,19 +1,9 @@
 const express = require('express');
-const cors = require('cors');
 
 const router = express.Router();
-router.use(cors());
 
-router.get('/index.html', (req, res, next) => {
-  res.set({ 'Content-Type': 'text/html' });
-  res.set({ 'Access-Control-Allow-Origin': 'http://localhost:3000' });
-  next();
-});
+router.get('/index.html', (req, res) => {});
 
-router.get('/data.json', (req, res, next) => {
-  res.set({ 'Content-Type': 'application/json' });
-  res.set({ 'Access-Control-Allow-Origin': '*' });
-  next();
-});
+router.get('/data.json', (req, res) => {});
 
 module.exports = router;
