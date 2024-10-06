e4df0ad260fe6d8173844fb62c7248024ca5d52c
stelasusanina <114248714+stelasusanina@users.noreply.github.com>
2024-10-06 14:48:08 +0300

    Added tests for validating CORS behavior

--- ExpressFirstApp/tests/app.test.js
+++ ExpressFirstApp/tests/app.test.js
@@ -49,3 +49,17 @@ describe('routes', () => {
     expect(response.text.trim()).toBe(expectedHtml.trim());
   });
 });
+
+describe('CORS tests', () => {
+  it('the Access-Control-Allow-Origin should be * for JSON files', async () => {
+    const response = await request(app).get('/static/data.json');
+    expect(response.headers['access-control-allow-origin']).toBe('*');
+  });
+
+  it('the Access-Control-Allow-Origin should be * for HTML files', async () => {
+    const response = await request(app).get('/static/index.html');
+    expect(response.headers['access-control-allow-origin']).toBe(
+      'http://localhost:3000'
+    );
+  });
+});
