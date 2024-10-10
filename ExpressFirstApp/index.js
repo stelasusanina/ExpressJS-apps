f4fe0e56e3aed14f397934fde41560158eff5377
stelasusanina <114248714+stelasusanina@users.noreply.github.com>
2024-10-10 11:23:52 +0300

    New endpoint logic added

--- ExpressFirstApp/index.js
+++ ExpressFirstApp/index.js
@@ -3,6 +3,8 @@ const dotenv = require('dotenv');
 const accessLogger = require('./middleware/accessLog');
 const corsMiddleware = require('./middleware/maintainCors');
 const durationLogger = require('./middleware/requestDuration');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 
 dotenv.config();
 const app = express();
@@ -16,6 +18,68 @@ app.use(express.static(staticDirectory));
 app.use(accessLogger);
 app.use(durationLogger);
 app.use(corsMiddleware);
+app.use(bodyParser.json());
+
+//Creating new todo files or appending to already created
+app.post('/todo', (req, res) => {
+  const { todoName, id, task } = req.body;
+  const log = `${id}. ${task} \n`;
+
+  fs.appendFile(`./todo/${todoName}.txt`, log, (err) => {
+    if (err) {
+      throw err;
+    }
+  });
+});
+
+//Getting the content from to do lists
+app.get('/todo', (req, res) => {
+  const { todoName } = req.body;
+
+  fs.readFile(`./todo/${todoName}.txt`, 'utf-8', (err, data) => {
+    if (err) {
+      console.error(err.message);
+    } else {
+      console.log(`${todoName}: `);
+      console.log(data);
+    }
+  });
+});
+
+//Deleting tasks
+app.delete('/todo', (req, res) => {
+  const { todoName, id } = req.body;
+
+  //1. Reading the whole content
+  fs.readFile(`./todo/${todoName}.txt`, 'utf-8', (err, data) => {
+    if (err) {
+      console.error(err.message);
+    } else {
+      //2. Filtering the lines to be removed
+      const lines = data.split('\n');
+      const filteredLines = lines.filter((line) => !line.startsWith(`${id}.`));
+      const updatedData = filteredLines.join('\n');
+
+      //3. Deleting the whole file
+      fs.unlink(`./todo/${todoName}.txt`, (err) => {
+        if (err) {
+          throw err;
+        }
+        console.log('File deleted!');
+      });
+
+      //4. Creating the file once again only with the filtered content
+      fs.appendFile(`./todo/${todoName}.txt`, updatedData, (err) => {
+        if (err) {
+          throw err;
+        }
+      });
+
+      console.log(`${todoName}: `);
+      console.log(data);
+    }
+  });
+});
 
 app.use((req, res) => {
   res
