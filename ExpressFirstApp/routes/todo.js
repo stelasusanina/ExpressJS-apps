eebaa6d3bb4ce6ce27ccf462ca3ec4146f9387b7
stelasusanina <114248714+stelasusanina@users.noreply.github.com>
2024-10-14 13:27:25 +0300

    Updated todo logic to use JSON instead text files and moved it to routes directory

---
+++ ExpressFirstApp/routes/todo.js
@@ -0,0 +1,111 @@
+const express = require('express');
+const fs = require('fs');
+const router = express.Router();
+
+//Creating new todo files or appending to already created
+router.post('/todo', (req, res) => {
+  const { todoName, id, task } = req.body;
+  const newTodo = {
+    id,
+    task,
+  };
+
+  let todos = { tasks: [] };
+
+  if (!todoName || !id || !task) {
+    return res.status(400).send('Missing required fields');
+  }
+
+  // Read the existing JSON file
+  fs.readFile(`./todo/${todoName}.json`, 'utf-8', (err, data) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        console.log('File does not exist, creating a new one.');
+      } else {
+        console.error(err);
+        return res.status(500).send('Failed to read the file');
+      }
+    } else {
+      todos = JSON.parse(data);
+    }
+
+    todos.tasks.push(newTodo);
+
+    fs.writeFile(`./todo/${todoName}.json`, JSON.stringify(todos), (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send('Failed to write to the file');
+      }
+
+      res.sendStatus(200);
+    });
+  });
+});
+
+//Getting the content from to do lists
+router.get('/todo', (req, res) => {
+  const { todoName } = req.query;
+
+  if (!todoName) {
+    return res.status(400).send("Missing required field 'todoName'");
+  }
+
+  fs.readFile(`./todo/${todoName}.json`, 'utf-8', (err, data) => {
+    if (err) {
+      console.error(err.message);
+    } else {
+      console.log(`${todoName}: `);
+      console.log(JSON.parse(data));
+
+      res.send(JSON.parse(data));
+    }
+  });
+});
+
+//Deleting tasks
+router.delete('/todo', (req, res) => {
+  const { todoName, id } = req.body;
+
+  let todos = { tasks: [] };
+
+  if (!todoName || !id) {
+    return res.status(400).send('Missing required fields');
+  }
+
+  fs.readFile(`./todo/${todoName}.json`, 'utf-8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Failed to read the file');
+    } else {
+      todos = JSON.parse(data);
+    }
+
+    todos = todos.tasks.filter((t) => t.id !== id);
+
+    //Deleting the file if there are no tasks left
+    if (todos.length === 0) {
+      fs.unlink(`./todo/${todoName}.json`, (err) => {
+        if (err) {
+          console.error(err);
+        } else {
+          return res
+            .status(200)
+            .send(
+              `./todo/${todoName}.json was deleted because there are no tasks left`
+            );
+        }
+      });
+    } else {
+      fs.writeFile(`./todo/${todoName}.json`, JSON.stringify(todos), (err) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).send('Failed to write to the file');
+        }
+
+        res.sendStatus(200);
+      });
+    }
+  });
+});
+
+module.exports = router;
